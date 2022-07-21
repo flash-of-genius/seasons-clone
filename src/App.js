4e7b42b3500ba52d8089dd35123555b60f118465
flash-of-genius <amhdaar@gmail.com>
2022-07-21 07:30:31 +0100

    7 - Refactoring from Functional to Class Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err),
-  );
-  return <div>Hi there !!!</div>;
-};
+import React from "react";
 
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err),
+    );
+    return <div>Latitude:</div>;
+  }
+}
 export default App;
