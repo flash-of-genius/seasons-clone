e5061c7bfef9150016d08b751b8684b7e8fc28d7
flash-of-genius <amhdaar@gmail.com>
2022-08-02 16:47:03 +0100

    13 - Specifying Default Props

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Spinner = () => {
+const Spinner = (props) => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">loading ....</div>
+      <div className="ui big text loader">{props.message}</div>
     </div>
   );
+};
+
+Spinner.defaultProps = {
+  message: "Loading ....",
 };
 
 export default Spinner;
