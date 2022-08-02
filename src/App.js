5706fb29b7db2336e9877b43f8adae9435a0dce0
flash-of-genius <amhdaar@gmail.com>
2022-08-02 16:33:09 +0100

    12 - Showing a Loading Spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -23,7 +24,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!!!</div>;
+    return <Spinner />;
   }
 }
 export default App;
